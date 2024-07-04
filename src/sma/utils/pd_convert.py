1339f9cb7b34bdc6c1022d506a0566c7663f1ef7
Lee <120439693+lygitdata@users.noreply.github.com>
2024-07-04 09:38:34 -0400

    Update pd_convert.py

--- src/sma/utils/pd_convert.py
+++ src/sma/utils/pd_convert.py
@@ -6,17 +6,15 @@ from .rgb_to_hex_vectorized import rgb_to_hex_vectorized
 
 def pd_convert(np_img):
     """
-    Convert images from URLs to a dictionary of Pandas DataFrames with RGB and hex values.
+    Convert Numpy Image Arrays to a Dictionary of Pandas DataFrames with RGB and Hex Values.
 
-    This function takes in a dictionary where keys are image names and values are URLs to images.
-    It reads each image from the provided URLs using `np_convert`. For each image, it reshapes
-    the image to ensure it has an RGB structure (-1, 3), converts each RGB triplet to a hex color
-    code, and combines the RGB values and hex codes into a DataFrame with columns "Red", "Green",
-    "Blue", and "hex". The RGB columns are ensured to be of integer type. The DataFrame for each
-    image is then appended to a dictionary.
+    This function takes in a dictionary where keys are image names and values are numpy arrays representing images.
+    For each image, it reshapes the image to ensure it has an RGB structure (-1, 3), converts each RGB triplet to a hex color
+    code, and combines the RGB values and hex codes into a DataFrame with columns "Red", "Green", "Blue", and "hex".
+    The RGB columns are ensured to be of integer type. The DataFrame for each image is then appended to a dictionary.
 
-    :param img_links: A dictionary where keys are image names and values are URLs to images.
-    :type img_links: dict
+    :param np_img: A dictionary where keys are image names and values are numpy arrays representing images.
+    :type np_img: dict
     :return: A dictionary of Pandas DataFrames, each containing RGB values and their corresponding hex codes.
     :rtype: dict
     """
