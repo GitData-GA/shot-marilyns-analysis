61de66c013ed418ab4bc5eb7894e093f0dbbc84f
Lee <120439693+lygitdata@users.noreply.github.com>
2024-07-04 21:23:09 -0400

    Update __init__.py

--- src/sma/roi/__init__.py
+++ src/sma/roi/__init__.py
@@ -3,3 +3,4 @@ from .find_color_range import find_color_range
 from .convert_to_numerical_tuple import convert_to_numerical_tuple
 from .extract import extract
 from .manual_fix import manual_fix
+from .knn_repair import knn_repair
