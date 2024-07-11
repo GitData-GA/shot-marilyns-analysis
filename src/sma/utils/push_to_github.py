d4a857ac9bc46bce750daef4375bdf67772bc6be
Lee <120439693+lygitdata@users.noreply.github.com>
2024-07-11 10:39:56 -0400

    Update push_to_github.py

--- src/sma/utils/push_to_github.py
+++ src/sma/utils/push_to_github.py
@@ -33,7 +33,7 @@ def push_to_github(
     gh_email = getpass.getpass(f"gh_email: \nNote: Your email address of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
     gh_username = getpass.getpass(f"\n\ngh_username: \nNote: Your username of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
     gh_password = getpass.getpass(f"\n\ngh_password: \nNote: Your password of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
-    gh_token = getpass.getpass(f"\n\ngh_token: \nNote: Your personal access token of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \nTo get a personal access token, please see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic. In the 'Select scopes' section, simply select all scopes.\n")
+    gh_token = getpass.getpass(f"\n\ngh_token: \nNote: Your personal access token of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \nTo get a personal access token, please see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic. \nIn the 'Select scopes' section, simply select all scopes.\n")
     delete_all_old_files_before_pushing = input("\n\nDelete all old files? (Y/N): \nNote: If you type Y, before pushing new files, the old files in the directory of your GitHub repo will be deleted first.\n")
 
     os.system(f"git config --global user.email {gh_email}")
