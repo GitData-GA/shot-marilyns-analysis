34e8c941c3792416eb5558bbcb4237f70b978ee3
Lee <120439693+lygitdata@users.noreply.github.com>
2024-07-10 22:41:09 -0400

    Update push_to_github.py

--- src/sma/utils/push_to_github.py
+++ src/sma/utils/push_to_github.py
@@ -27,7 +27,7 @@ def push_to_github(username, repository, branch, from_path, to_path):
     gh_username = getpass.getpass(f"\n\ngh_username: \nNote: Your username of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
     gh_password = getpass.getpass(f"\n\ngh_password: \nNote: Your password of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
     gh_token = getpass.getpass(f"\n\ngh_token: \nNote: Your personal access token of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \nTo get a personal access token, please see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic. In the 'Select scopes' section, simply select all scopes.\n")
-    delete_all_old_files_before_pushing = input("\n\nDelete all old files? (Y/N): \nNote: If you type Y, before pushing new files, the old files in the directory of your GitHub repo will be deleted first.")
+    delete_all_old_files_before_pushing = input("\n\nDelete all old files? (Y/N): \nNote: If you type Y, before pushing new files, the old files in the directory of your GitHub repo will be deleted first.\n")
 
     os.system(f"git config --global user.email {gh_email}")
     os.system(f"git config --global user.name {gh_username}")
