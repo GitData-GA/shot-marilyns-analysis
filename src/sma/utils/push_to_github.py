4f903d00873cc753d439033120c0ffce89bb1e12
Lee <120439693+lygitdata@users.noreply.github.com>
2024-07-13 16:06:24 -0400

    Update push_to_github.py

--- src/sma/utils/push_to_github.py
+++ src/sma/utils/push_to_github.py
@@ -1,12 +1,18 @@
 import os
 import getpass
 import textwrap
-from datetime import datetime 
+from datetime import datetime
 import pytz
 
 
 def push_to_github(
-    username, repository, branch, from_path, to_path, delete_local_branch=True
+    username,
+    repository,
+    branch,
+    from_path,
+    to_path,
+    delete_local_branch=True,
+    timezone="UTC",
 ):
     """
     Push Local Changes to GitHub Repository.
@@ -28,13 +34,25 @@ def push_to_github(
     :type to_path: str
     :param delete_local_branch: Whether to delete the local copy of the branch after pushing (default: True).
     :type delete_local_branch: bool
+    :param timezone: Timezone for the commit timestamp (default: 'UTC').
+    :type timezone: str
     :raises ValueError: If the input for deleting old files is not 'Y' or 'N'.
     """
-    gh_email = getpass.getpass(f"gh_email: \nNote: Your email address of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
-    gh_username = getpass.getpass(f"\n\ngh_username: \nNote: Your username of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
-    gh_password = getpass.getpass(f"\n\ngh_password: \nNote: Your password of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n")
-    gh_token = getpass.getpass(f"\n\ngh_token: \nNote: Your personal access token of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \nTo get a personal access token, please see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic. \nIn the 'Select scopes' section, simply select all scopes.\n")
-    delete_all_old_files_before_pushing = input("\n\nDelete all old files? (Y/N): \nNote: If you type Y, before pushing new files, the old files in the directory of your GitHub repo will be deleted first.\n")
+    gh_email = getpass.getpass(
+        f"gh_email: \nNote: Your email address of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n"
+    )
+    gh_username = getpass.getpass(
+        f"\n\ngh_username: \nNote: Your username of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n"
+    )
+    gh_password = getpass.getpass(
+        f"\n\ngh_password: \nNote: Your password of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \n"
+    )
+    gh_token = getpass.getpass(
+        f"\n\ngh_token: \nNote: Your personal access token of the GitHub account that can read and write https://github.com/{username}/{repository}/tree/{branch} \nTo get a personal access token, please see https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic. \nIn the 'Select scopes' section, simply select all scopes.\n"
+    )
+    delete_all_old_files_before_pushing = input(
+        "\n\nDelete all old files? (Y/N): \nNote: If you type Y, before pushing new files, the old files in the directory of your GitHub repo will be deleted first.\n"
+    )
 
     os.system(f"git config --global user.email {gh_email}")
     os.system(f"git config --global user.name {gh_username}")
@@ -55,7 +73,9 @@ def push_to_github(
     os.system(f"cp {from_path}/* {branch}_copy/{to_path}")
     os.chdir(f"{branch}_copy")
     os.system("git add .")
-    os.system(f"git commit -m '{gh_username}@colab {datetime.now(pytz.utc).strftime('%Y%m%d%H%M%S')} UTC'")
+    os.system(
+        f"git commit -m '{gh_username}@colab {datetime.now(pytz.timezone(timezone)).strftime('%Y%m%d%H%M%S')} {timezone}'"
+    )
     os.system(f"git push origin {branch}")
     os.chdir("..")
     if delete_local_branch:
