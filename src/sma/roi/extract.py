014d6189d6ab319cdb2432fad584da04312f67a9
Lee <120439693+lygitdata@users.noreply.github.com>
2024-09-15 18:41:59 +0200

    Update extract.py

--- src/sma/roi/extract.py
+++ src/sma/roi/extract.py
@@ -39,7 +39,7 @@ def extract(np_img, key, img_idx, extraction_name, param, fix=None, verbose=Fals
     :return: The extracted image with shape as (960, 960, 3).
     :rtype: numpy.ndarray
     """
-    plt.rcParams['font.family'] = 'Liberation Serif'
+    plt.rcParams['font.family'] = 'STIXGeneral'
     img = np_img[key].reshape(960, 960, 3)
 
     color_range_min = (None, None, None)
