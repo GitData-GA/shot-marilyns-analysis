cad7c7c0a877a4edd4927c34fc28ccd93147de95
Lee <120439693+lygitdata@users.noreply.github.com>
2024-09-15 18:39:59 +0200

    Update bar.py

--- src/sma/plot/bar.py
+++ src/sma/plot/bar.py
@@ -39,7 +39,7 @@ def bar(
     :return: None
     :rtype: None
     """
-    plt.rcParams['font.family'] = 'Liberation Serif'
+    plt.rcParams['font.family'] = 'STIXGeneral'
     idx = 0
     for key, img in zip(pd_img.keys(), pd_img.values()):
         if isinstance(kmeans, dict):
